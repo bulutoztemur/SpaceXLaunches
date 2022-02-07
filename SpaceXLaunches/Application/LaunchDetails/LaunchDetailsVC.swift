bcbde817bf067cb529cf6f546509c5e69086acfa
ALAATTIO <alaattinbulut.oztemur@intertech.com.tr>
2022-02-07 16:06:56 +0300

    YTplayer and imageslideshow views added

--- SpaceXLaunches/Application/LaunchDetails/LaunchDetailsVC.swift
+++ SpaceXLaunches/Application/LaunchDetails/LaunchDetailsVC.swift
@@ -6,9 +6,28 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import youtube_ios_player_helper
+import ImageSlideshow
 
 class LaunchDetailsVC: UIViewController {
     private let viewModel = LaunchDetailsVM()
+    
+    @IBOutlet weak var ytPlayerView: YTPlayerView!
+    @IBOutlet weak var imageSlideShow: ImageSlideshow! {
+        didSet {
+            let labelPage = LabelPageIndicator()
+            labelPage.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+            labelPage.attributedText = NSAttributedString(string: labelPage.text ?? "",
+                                                          attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 14)])
+            labelPage.textColor = .white
+            imageSlideShow.pageIndicator = labelPage
+            imageSlideShow.pageIndicatorPosition = PageIndicatorPosition(horizontal: .right(padding: 10), vertical: .customBottom(padding: 10))
+            imageSlideShow.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(didTapImage)))
+        }
+    }
+    
     private let launchId: String
 
     init(id: String) {
@@ -22,9 +41,38 @@ class LaunchDetailsVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        bind()
         viewModel.fetchDetails(id: launchId)
     }
 
+    @objc func didTapImage() {
+        imageSlideShow.presentFullScreenController(from: self)
+    }
+}
 
-
+//MARK:- Binding Operations
+extension LaunchDetailsVC {
+    func bind() {
+        viewModel.launch
+            .flatMap { $0.map(Observable.just) ?? Observable.empty() }
+            .subscribe(onNext: { [weak self] launch in
+                if let links = launch.links {
+                    if let youtubeLink = links.videoLink {
+                        let splitted = youtubeLink.split(separator: "/")
+                        let videoId = String(splitted[splitted.count - 1])
+                        self?.ytPlayerView.load(withVideoId: videoId)
+                    } else {
+                        self?.ytPlayerView.isHidden = true
+                    }
+                    
+                    if let inputs = links.flickrImages?.compactMap({ AlamofireSource(urlString: $0!) }), !inputs.isEmpty {
+                        self?.imageSlideShow.setImageInputs(inputs)
+                    } else {
+                        self?.imageSlideShow.isHidden = true
+                    }
+                    
+                }
+            })
+            .disposed(by: viewModel.disposeBag)
+        }
 }
